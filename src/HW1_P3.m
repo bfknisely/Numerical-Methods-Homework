4ccb2ce58a84811a5abaf9923e74ee55f909f589
bfknisely <35206584+bfknisely@users.noreply.github.com>
2018-01-24 10:05:12 -0500

    Completed HW1, restructured folders

--- src/HW1_P3.m
+++ src/HW1_P3.m
@@ -33,15 +33,17 @@ cds4 = zeros(n, 2);  % 4th order central difference
 for i = 1:n
     exact = -sin(0.3);  % Compute analytical derivative
     % Approximate derivative with each scheme
-    bds1(i, 1) = (cos(x)-cos(x-h(i))) / h(i);  
-    cds2(i, 1) = (cos(x+h(i))-cos(x-h(i))) / (2*h(i)); 
+    fds1(i, 1) = (cos(x+h(i))-cos(x)) / h(i);
+    bds1(i, 1) = (cos(x)-cos(x-h(i))) / h(i);
+    cds2(i, 1) = (cos(x+h(i))-cos(x-h(i))) / (2*h(i));
     fds3(i, 1) = (-cos(x+2*h(i))+6*cos(x+h(i))-3*cos(x)-2*cos(x-h(i))) /...
         (6*h(i));
     bds3(i, 1) = (2*cos(x+h(i))+3*cos(x)-6*cos(x-h(i))+cos(x-2*h(i))) / ...
         (6*h(i));
     cds4(i, 1) = (-cos(x+2*h(i))+8*cos(x+h(i))-8*cos(x-h(i))+...
-        cos(x-2*h(i))) / (12*h(i)); 
+        cos(x-2*h(i))) / (12*h(i));
     % Compute errors for each scheme
+    fds1(i, 2) = fds1(i, 1) - exact;
     bds1(i, 2) = bds1(i, 1) - exact;
     cds2(i, 2) = cds2(i, 1) - exact;
     fds3(i, 2) = fds3(i, 1) - exact;
@@ -50,23 +52,25 @@ for i = 1:n
 end
 
 figure(1);  % Create figure to show error
-loglog(h, abs(bds1(:, 2)), 'ko', h, abs(cds2(:, 2)), 'rx',...
-    h, abs(fds3(:, 2)), 'b*', h, abs(bds3(:, 2)), 'ms', ...
-    h, abs(cds4(:, 2)), 'kp'); 
+loglog(h, abs(fds1(:, 2)), 'r+-', h, abs(bds1(:, 2)), 'ko--', ...
+    h, abs(cds2(:, 2)), 'rx:', h, abs(fds3(:, 2)), 'b*-', ...
+    h, abs(bds3(:, 2)), 'ms--', h, abs(cds4(:, 2)), 'kp:'); 
 xlabel('Spacing \it{h}'); ylabel('|Error|'); 
-legend('BDS1', 'CDS2', 'FDS3', 'BDS3', 'CDS4', 'location', 'southwest'); 
+legend('FDS1', 'BDS1', 'CDS2', 'FDS3', 'BDS3', 'CDS4', ...
+    'location', 'southwest'); 
 set(gca, 'fontsize', 16);
 xlim([1e-13, 10]);
+set(gcf, 'outerposition', [50 50 850 650])
 
 fid = fopen('results.txt', 'w');  % Open a file to store results
 
 % Print error results
-fprintf(fid,'%-2s|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s\n', ...
-    'i', '      h', ' Error:BDS1', ' Error:CDS2', ' Error:FDS3', ...
-    ' Error:BDS3', ' Error:CDS4');
+fprintf(fid,'%-2s|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s|%-12s\n', ...
+    'i', '      h', ' Error:FDS1', ' Error:BDS1', ' Error:CDS2', ...
+    ' Error:FDS3', ' Error:BDS3', ' Error:CDS4');
 for i = 1:n
     fprintf(fid,'%2.0f,%12.5e,%12.5e,%12.5e,%12.5e,%12.5e,%12.5e,%12.5e', ...
-    i, h(i), bds1(i, 2), cds2(i, 2), fds3(i, 2), bds3(i, 2), cds4(i, 2));
+    i, h(i), fds1(i, 2), bds1(i, 2), cds2(i, 2), fds3(i, 2), bds3(i, 2), cds4(i, 2));
     fprintf(fid,'\n');
 end
 
